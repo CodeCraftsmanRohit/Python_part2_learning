fa06de74edf5f3a81064cb8133861a1571cbfe8d
Rohit Kumar <rohitkumar.ak51@gmail.com>
2025-08-02 15:46:24 +0530

    pymongo

--- api_handling_free/freeapi_username.py
+++ api_handling_free/freeapi_username.py
@@ -2,8 +2,8 @@ import requests
 
 def fetch_random_user_freeapi():
     url="https://api.freeapi.app/api/v1/public/randomusers/user/random"
-    response=requests.get(url)
-    data=response.json()
+    response=requests.get(url) #string form me h
+    data=response.json()  # string to json
 
     if data["success"] and "data" in data:
         user_data=data["data"]
